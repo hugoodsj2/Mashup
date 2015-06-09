7035412f5ab6772c0bce95ee92e515ad54d8e24f
BarbaraAbreu <BarbaraAbreu@users.noreply.github.com>
2015-06-09 18:29:10 -0300

    Arquivo .sql da criação da tabela

---
+++ mashup.sql
@@ -0,0 +1,3 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO"; SET time_zone = "+00:00";
+
+CREATE TABLE IF NOT EXISTS localidade ( nomeCorretor varchar(50) NOT NULL, lat varchar(50) NOT NULL, lng varchar(50) NOT NULL ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
